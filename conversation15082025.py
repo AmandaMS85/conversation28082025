9a34a8d7851e5e44ac24e7e4890e89f833d9127f
Amanda Martins Soares <amanda_soares85@hotmail.com>
2025-08-28 19:43:19 -0300

    Add files via upload

---
+++ conversation15082025.py
@@ -0,0 +1,24 @@
+name = input("Hello what's your name? ")
+
+print(f"Nice to meet you, {name}")
+
+
+print("How old are you?") 
+age = int(input())
+
+bot_age = 3
+age_difference = age - bot_age
+
+print(f"Wow, I Have {bot_age}" )
+
+print(f"You have {age_difference} than me!")
+
+color = input("What's your favorite color? ")
+
+person_color = input()
+
+pc_color = "pink"
+
+print(f"Cool, I like this color too but my favorite color is {pc_color}.")
+
+
